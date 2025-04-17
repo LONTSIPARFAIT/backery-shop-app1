5807c55637cb1696f1c39ec7a1863f551123bf51
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-17 16:16:05 +0100

    Boulangerie

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
 
-import { TabsPage } from './tabs.page';
+// import { TabsPage } from './tabs.page';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: TabsPage
-  }
-];
+// const routes: Routes = [
+//   {
+//     path: '',
+//     component: TabsPage
+//   }
+// ];
 
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-})
-export class TabsPageRoutingModule {}
+// @NgModule({
+//   imports: [RouterModule.forChild(routes)],
+//   exports: [RouterModule],
+// })
+// export class TabsPageRoutingModule {}
